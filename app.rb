6ddb946c580263dfecbd3763ab7f27441d5675a4
Júlio Bueno <julioce@gmail.com>
2013-02-09 17:37:42 -0200

    *Adição de Hash de associação de codon para aminoácido e verificação de tamanho de códon múltiplo de 3 e formatação de dna para DNA

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'sinatra'
 
 get '/' do
@@ -8,15 +10,26 @@ get '/' do
 end
 
 post '/' do
-	
-	# Gets de DN value
-	@dna = params[:dna]
 
-	# Loads the result view
-	erb :'result'
+	# Gets de DNA value
+	@dna = params[:dna].upcase
+
+	#Verifies if it's mod 3 correct codon
+	if (@dna.size % 3) != 0
+		@error_message = "Seu DNA não possui o número correto de códons, múltiplo de 3."
+		erb :'error'
+	else
+		# Associates de codon with the respective aminoacid 
+		@codon_to_amioacid_hash ={:ATG=> "M", :TCG=> "S", :GTA=> "V", :GAA=> "E", :GAC=> "D", :ATT=> "I", :TTT=> "F", :AAA=> "K", :ACT=> "T", :ATG=> "M", :TAT=> "Y", :GAA=> "E", :TCC=> "S", :GTA=> "V", :CCG=> "P", :GAA=> "E", :CCC=> "P", :AAG=> "K", :AAG=> "K", :AAT=> "K", :TGC=> "N"}
+		
+
+		# Loads the result view
+		erb :'result'
+	end
 
 end
 
 error do
+	@error_message = "O administrador do sistema será notificado."
 	erb :'error'
 end
